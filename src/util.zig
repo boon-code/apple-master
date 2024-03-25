2751ee8b715c9a0e6cb6cd37d4e59d4d4aa87db8
Manuel Huber <manuel.h87@gmail.com>
2024-03-25 22:18:54 +0100

    Fix util.getRandom()
    There seems to be a bug in rl.GetRandomValue() which limits the usable
    range from 0 to 2**31-1.

--- src/util.zig
+++ src/util.zig
@@ -2,15 +2,26 @@
 const std = @import("std");
 const rl = @import("raylib");
 
+// BUG: rl.GetRandomValue seems to be limited to 2**31 - 1 in this case
+const I32_MIN = 0;
+const I32_MAX = std.math.maxInt(i32) - 1;
+
 pub fn f32FromInt(v: anytype) f32 {
     return @as(f32, @floatFromInt(v));
 }
 
 pub fn getRandom(comptime T: type, min: T, max: T) T {
-    const mi = std.math.minInt(i32);
-    const ma = std.math.maxInt(i32);
-    const intValue = rl.getRandomValue(std.math.minInt(i32), std.math.maxInt(i32));
+    return getRandomInner(T, getRandomI32FullRange(), min, max);
+}
 
-    const prop: T = @as(T, @floatFromInt(intValue)) / @as(T, @floatFromInt(ma - mi));
+inline fn getRandomI32FullRange() i32 {
+    return rl.getRandomValue(I32_MIN, I32_MAX);
+}
+
+inline fn getRandomInner(comptime T: type, randInt: i32, min: T, max: T) T {
+    //const range: T = @as(T, @floatFromInt(I32_MAX)) - @as(T, @floatFromInt(I32_MIN));
+    // BUG: see getRandomI32FullRange
+    const range: T = @as(T, @floatFromInt(I32_MAX)) - @as(T, @floatFromInt(I32_MIN));
+    const prop: T = @as(T, @floatFromInt(randInt)) / range;
     return min + (max - min) * prop;
 }
