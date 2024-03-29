44d91a345560f83f089a630bd49561b872553840
Manuel Huber <manuel.h87@gmail.com>
2024-03-29 16:11:50 +0100

    refactor: Fix code styling in game

--- src/game.zig
+++ src/game.zig
@@ -17,21 +17,21 @@ pub const State = struct {
     const AnimationIndex = sprite.SpriteSheetUniform.Index.Animated;
 
     // Sprites
-    backgroundTexture: rl.Texture2D,
+    background: rl.Texture2D,
 
-    appleManager: apple.AppleManager,
+    apple_manager: apple.AppleManager,
 
-    healthBack: rl.Texture2D,
-    healthFront: rl.Texture2D,
+    health_back: rl.Texture2D,
+    health_front: rl.Texture2D,
 
     player: player.Player,
-    plusEffect: plus.BonusEffect,
+    plus_effect: plus.BonusEffect,
 
     delta: f32,
     time: f64,
-    baseTime: f64,
+    base_time: f64,
     paused: bool,
-    showKeyMap: bool,
+    show_key_map: bool,
 
     health: f32,
     hurt: f32,
@@ -43,43 +43,39 @@ pub const State = struct {
 
     pub fn init(allocator: std.mem.Allocator) !Self {
         const time: f64 = 0.0;
-        const baseTime = rl.getTime();
-        const backgroundTexture = sprite.loadTextureEmbed(constants.TEXTURE_DIR ++ "BG.png");
-        errdefer rl.unloadTexture(backgroundTexture);
+        const base_time = rl.getTime();
+        const background = sprite.loadTextureEmbed(constants.TEXTURE_DIR ++ "BG.png");
+        errdefer rl.unloadTexture(background);
 
         var man = try apple.AppleManager.init(allocator, time);
         errdefer man.unload();
 
         // Health bar
-        const healthBack = sprite.loadTextureEmbed(constants.TEXTURE_DIR ++ "STR1.png");
-        errdefer rl.unloadTexture(healthBack);
-        const healthFront = sprite.loadTextureEmbed(constants.TEXTURE_DIR ++ "STR2.png");
-        errdefer rl.unloadTexture(healthFront);
-        std.debug.assert(healthBack.width == healthFront.width);
-        std.debug.assert(healthBack.height == healthFront.height);
-
-        // ++ Animation
-        var plusSpriteSheet = sprite.SpriteSheetUniform.initFromEmbeddedFile(constants.TEXTURE_DIR ++ "PL.png", 1, 18);
-        errdefer plusSpriteSheet.unload();
+        const health_back = sprite.loadTextureEmbed(constants.TEXTURE_DIR ++ "STR1.png");
+        errdefer rl.unloadTexture(health_back);
+        const health_front = sprite.loadTextureEmbed(constants.TEXTURE_DIR ++ "STR2.png");
+        errdefer rl.unloadTexture(health_front);
+        std.debug.assert(health_back.width == health_front.width);
+        std.debug.assert(health_back.height == health_front.height);
 
         var p = player.Player.init();
         errdefer p.unload();
 
-        var plusEffect = try plus.BonusEffect.init(allocator);
-        errdefer plusEffect.unload();
+        var plus_effect = try plus.BonusEffect.init(allocator);
+        errdefer plus_effect.unload();
 
         return Self{
-            .backgroundTexture = backgroundTexture,
-            .appleManager = man,
-            .healthBack = healthBack,
-            .healthFront = healthFront,
+            .background = background,
+            .apple_manager = man,
+            .health_back = health_back,
+            .health_front = health_front,
             .player = p,
-            .plusEffect = plusEffect,
+            .plus_effect = plus_effect,
             .delta = 0,
             .time = time,
-            .baseTime = baseTime,
+            .base_time = base_time,
             .paused = false,
-            .showKeyMap = true,
+            .show_key_map = true,
             .health = 100.0,
             .hurt = 0.0,
             .score = 0,
@@ -91,7 +87,7 @@ pub const State = struct {
         if (self.paused) {
             self.delta = 0.0;
         } else {
-            self.time = rl.getTime() - self.baseTime;
+            self.time = rl.getTime() - self.base_time;
             self.delta = rl.getFrameTime();
         }
     }
@@ -106,7 +102,7 @@ pub const State = struct {
 
     fn togglePause(self: *Self) void {
         if (self.paused) { // unpause
-            self.baseTime = rl.getTime() - self.time;
+            self.base_time = rl.getTime() - self.time;
         }
 
         self.paused = !self.paused;
@@ -124,7 +120,7 @@ pub const State = struct {
         }
 
         if (rl.isKeyPressed(.key_k)) {
-            self.showKeyMap = !self.showKeyMap;
+            self.show_key_map = !self.show_key_map;
         }
 
         if (rl.isKeyPressed(.key_d)) {
@@ -138,7 +134,7 @@ pub const State = struct {
         if (self.health <= 0.0) {
             return;
         }
-        _ = self.appleManager.update(self.time);
+        _ = self.apple_manager.update(self.time);
     }
 
     pub fn isDebug(self: Self) bool {
@@ -166,7 +162,7 @@ pub const State = struct {
                 self.health = 100.0;
             }
             std.debug.print("Spawn a plus here: {d} {d}\n", .{ apple_.position.x, apple_.position.y });
-            self.plusEffect.spawn(apple_.position, self.time);
+            self.plus_effect.spawn(apple_.position, self.time);
         }
     }
 
@@ -185,11 +181,11 @@ pub const State = struct {
 
     pub fn draw(self: *Self) void {
         // Background
-        rl.drawTexture(self.backgroundTexture, 0, 0, rl.Color.white);
+        rl.drawTexture(self.background, 0, 0, rl.Color.white);
         self.drawHealthBar();
 
         // Key map
-        if (self.showKeyMap) {
+        if (self.show_key_map) {
             Self.drawKeyInfo();
         }
 
@@ -199,10 +195,10 @@ pub const State = struct {
         }
 
         // Apple
-        self.appleManager.drawUpdate(self.time, self.delta, self.player, self);
+        self.apple_manager.drawUpdate(self.time, self.delta, self.player, self);
 
         // Bonus effect
-        self.plusEffect.drawAndUpdate(self.time);
+        self.plus_effect.drawAndUpdate(self.time);
 
         // Basket
         self.player.draw(self.debug);
@@ -217,12 +213,12 @@ pub const State = struct {
     }
 
     fn drawKeyInfo() void {
-        const OFF_X = 520;
-        const OFF_Y = 20;
-        const FONT_SIZE = 20;
-        const SPACE_Y = 25;
-        const COLOR = rl.Color.light_gray.alpha(0.7);
-        const TEXT = [_][:0]const u8{
+        const off_x = 520;
+        const off_y = 20;
+        const font_size = 20;
+        const space_y = 25;
+        const text_color = rl.Color.light_gray.alpha(0.7);
+        const key_map = [_][:0]const u8{
             "Key map:",
             "- left arrow to move left",
             "- right arrow to move right",
@@ -232,8 +228,8 @@ pub const State = struct {
             "- f to toggle full screen",
             "- q to quit",
         };
-        inline for (TEXT, 0..) |text, i| {
-            rl.drawText(text, OFF_X, OFF_Y + SPACE_Y * @as(i32, i), FONT_SIZE, COLOR);
+        inline for (key_map, 0..) |text, i| {
+            rl.drawText(text, off_x, off_y + space_y * @as(i32, i), font_size, text_color);
         }
     }
 
@@ -251,36 +247,35 @@ pub const State = struct {
     }
 
     pub fn unload(self: *Self) void {
-        rl.unloadTexture(self.backgroundTexture);
-        rl.unloadTexture(self.healthBack);
-        rl.unloadTexture(self.healthFront);
+        rl.unloadTexture(self.background);
+        rl.unloadTexture(self.health_back);
+        rl.unloadTexture(self.health_front);
         self.appleSpriteSheet.unload();
         self.healthSpriteSheet.unload();
-        self.plusSpriteSheet.unload();
         self.player.unload();
     }
 
     fn drawHealthBar(self: Self) void {
-        const barY = 100.0;
-        const frameWidth = f32FromInt(self.healthBack.width);
-        const frameHeight = f32FromInt(self.healthBack.height);
+        const bar_y = 100.0;
+        const frame_width = f32FromInt(self.health_back.width);
+        const frame_height = f32FromInt(self.health_back.height);
         const origin = rl.Vector2.init(0.0, 0.0);
-        const dstWidth = frameWidth * 2.0;
-        const dstHeight = frameHeight * 2.0;
+        const dst_width = frame_width * 2.0;
+        const dst_height = frame_height * 2.0;
         // Back
-        const dstBack = rl.Rectangle.init(constants.HEALTH_BAR_X, barY, dstWidth, dstHeight);
-        const srcBack = rl.Rectangle.init(0, 0, frameWidth, frameHeight);
+        const dst_back = rl.Rectangle.init(constants.HEALTH_BAR_X, bar_y, dst_width, dst_height);
+        const src_back = rl.Rectangle.init(0, 0, frame_width, frame_height);
         // Front
         const f = (100.0 - self.health) / 100.0;
-        var srcDy = frameHeight * f;
-        var dstDy = dstHeight * f;
-        const dstFront = rl.Rectangle.init(constants.HEALTH_BAR_X, barY + dstDy, dstWidth, dstHeight - dstDy);
-        const srcFront = rl.Rectangle.init(0, srcDy, frameWidth, frameHeight - srcDy);
+        var src_dy = frame_height * f;
+        var dst_dy = dst_height * f;
+        const dstFront = rl.Rectangle.init(constants.HEALTH_BAR_X, bar_y + dst_dy, dst_width, dst_height - dst_dy);
+        const srcFront = rl.Rectangle.init(0, src_dy, frame_width, frame_height - src_dy);
 
         self.drawHealtText();
 
-        rl.drawTexturePro(self.healthBack, srcBack, dstBack, origin, 0.0, rl.Color.white);
-        rl.drawTexturePro(self.healthFront, srcFront, dstFront, origin, 0.0, rl.Color.white);
+        rl.drawTexturePro(self.health_back, src_back, dst_back, origin, 0.0, rl.Color.white);
+        rl.drawTexturePro(self.health_front, srcFront, dstFront, origin, 0.0, rl.Color.white);
     }
 
     fn drawHealtText(self: Self) void {
